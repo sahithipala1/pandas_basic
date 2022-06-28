ebc9a14dcce2503bbad5553d305fca57db30031c
kranthigy <kranthi@goodyear.com>
2022-06-28 13:15:11 -0400

    basics of pandas data

---
+++ basics.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+dataframe = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
+
+x = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']})
+
+print(dataframe)
+print(x)
