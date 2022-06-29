9cb77238eb347844908a6285767d68831f11df03
kranthigy <kranthi@goodyear.com>
2022-06-29 15:12:08 -0400

    basics of pandas data

--- tables.py
+++ tables.py
@@ -5,3 +5,7 @@ office_supplies = pd.read_csv('../pandas_basic/P1-OfficeSupplies.csv', index_col
 print(office_supplies)
 print(office_supplies.shape)
 print(office_supplies.head())
+print(office_supplies['Rep'])
+print(office_supplies['Rep'][0])
+print(office_supplies.iloc[0])
+print(office_supplies.loc['Rep'])
